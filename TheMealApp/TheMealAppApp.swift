74c83a6b87c33db4ffb73ed2347ea6f6301d848a
Manuel Duarte <carlosd99m@gmail.com>
2024-11-03 11:59:05 -0600

    Initial Commit

---
+++ TheMealApp/TheMealAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMealAppApp.swift
+//  TheMealApp
+//
+//  Created by Manuel Duarte on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TheMealAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
